fb26375fc11aecd7ffa9f857c9b6e4fab6153775
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-12-12 15:24:15 +0100

    Add init function.

--- form_tracker.js
+++ form_tracker.js
@@ -1,5 +1,11 @@
 var FormTracker = {
-  init: function(gtag) {
+  init: function(gtag, form_id) {
     this.$gtag = gtag;
+    this.$formHistory = [];
+    this.$formIsSubmitted = false;
+    this.$formId = form_id;
+    this.$form = document.getElementById(form_id);
+
+  },
   }
 }

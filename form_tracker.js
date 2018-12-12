5aad5955464334b1551452757b13c4432114bd28
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-12-12 15:24:41 +0100

    Attach events to form fields.

--- form_tracker.js
+++ form_tracker.js
@@ -6,6 +6,33 @@ var FormTracker = {
     this.$formId = form_id;
     this.$form = document.getElementById(form_id);
 
+    this.attachEvents();
+  },
+  attachEvents: function() {
+    let that = this;
+    document.querySelectorAll('select').forEach(function(el) {
+      el.addEventListener('change', function(e) { return that.onFieldChange(e); });
+    });
+    document.querySelectorAll('input, textarea').forEach(function(el) {
+      el.addEventListener('input', function(e) { return that.onFieldInput(e); });
+    });
+    this.$form.addEventListener('submit', function(e) { return that.onFormSubmit(e) });
+  },
+  onFieldChange(event) {
+    let field = event.target;
+    let field_name = event.target.name;
+    console.log(field_name);
+  },
+  onFieldInput(event) {
+    let field = event.target;
+    let field_name = event.target.name;
+    console.log(field_name);
+  },
+  onFormSubmit(event) {
+    console.log("Form Submit");
+  },
+  sendEvent: function() {
+
   },
   }
 }
